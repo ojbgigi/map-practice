92397f325402bc5c580ab873ddd7a5d546701e1b
ojbgigi <ojbgigi@velocitylight.net>
2020-05-19 17:05:13 +0800

    可讀取本機json檔案並顯示於地圖上

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var map = L.map('map', {
-  center: [25.0534891, 121.5857258],
-  zoom: 20
+  center: [24.990011, 121.270653],
+  zoom: 12
 });
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,39 +8,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var markers = new L.MarkerClusterGroup().addTo(map);
-var requestURL =
-  'https://raw.githubusercontent.com/ojbgigi/map-practice/master/car_export.json';
+var requestURL = '/10812_2.json';
 var xhr = new XMLHttpRequest();
 
 xhr.open('GET', requestURL);
 xhr.send();
-// xhr.onload = function() {
-//   if (this.readyState === 4 && this.status === 200) {
-//     var data = JSON(this.responseText);
-//     console.log(data);
-//     //   for (let i = 0; data.length > i; i++) {
-//     //     markers.addLayer(
-//     //       L.marker([
-//     //         data[i].geometry.coordinates[1],
-//     //         data[i].geometry.coordinates[0]
-//     //       ]).bindPopup(data[i].records.breau)
-//     //     );
-//     //   }
-//     // }
-//   }
-// };
 xhr.onload = function() {
-  var data = JSON.parse(xhr.responseText);
-  console.log('顯示有沒有抓到api' + data);
-  // console.log('顯示responseText' + request.responseText);
-  // for (let i = 0; data.length > i; i++) {
-  //   markers.addLayer(
-  //     L.marker([data[i].records.lat, data[i].records.lon]).bindPopup(
-  //       data[i].records.breau
-  //     )
-  //   );
-  // }
+  if (this.readyState === 4 && this.status === 200) {
+    var response = JSON.parse(xhr.responseText);
+    var data = response.result.records;
+    for (let i = 0; data.length > i; i++) {
+      L.marker([data[i].lat, data[i].lon]).addTo(map);
+    }
+  }
 };
-
-// map.addLayer(markers);
